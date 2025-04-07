e593839ee5fc89813ef65d367b03bafe38dbf13b
bstocker <boris.stocker@gmail.com>
2025-04-07 11:40:41 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
-from flask import render_template
-from flask import json                                                                                                                                     
+
 app = Flask(__name__)
 
-@app.route('/<int:valeur>')
-def exercice(n):
-    count = ['0', '1'] if n > 1 else ['0']
+@app.route('/<int:n>')
+def fibo(n):
+    a, b = 0, 1
+    res = str(a)  # Initialisation avec le premier terme
+    
+    for _ in range(1, min(n, 50)):  # Limité à 50 termes pour la sécurité
+        res += f", {b}"
+        a, b = b, a + b
+    
+    return res
 
-    for _ in range(2, n):
-        count.append(str(int(count[-1]) + int(count[-2])))
-
-    return ', '.join(count[:n])
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
