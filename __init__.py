88c12af7248132d1b14ade040a7dec877838bfa2
bstocker <boris.stocker@gmail.com>
2025-04-07 11:33:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,16 +6,12 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    val = [0, 1]
-    result = []
-    result.append(val[0])
-    result.append(val[1])
-    
-    for _ in range(2, valeur):
-        result.append(val[0] + val[1])
-        val[1] += val[0]
-        val[0] = val[1] - val[0]
-    return jsonify(result)
+    count = ['0', '1'] if n > 1 else ['0']
+
+    for _ in range(2, n):
+        count.append(str(int(count[-1]) + int(count[-2])))
+
+    return ', '.join(count[:n])
 
 if __name__ == "__main__":
   app.run(debug=True) 
