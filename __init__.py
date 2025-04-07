0aabbda01ea9f5f0d155d47ff8bfeb5276ede415
bstocker <boris.stocker@gmail.com>
2025-04-07 12:45:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,16 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/<int:n>')
-def fibo(n):
-    a, b = 0, 1
-    res = str(a)  # Initialisation avec le premier terme
-    
-    for _ in range(1, min(n, 50)):  # Limité à 50 termes pour la sécurité
-        res += f", {b}"
-        a, b = b, a + b
-    
-    return res
+@app.route('/somme/<path:valeurs>')
+def somme(valeurs):
+    liste_nombres = valeurs.split('/')
+    liste_nombres = [int(n) for n in liste_nombres]
+    res = sum(liste_nombres)
+    return str(res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
