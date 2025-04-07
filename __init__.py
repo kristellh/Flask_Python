30dc67688452d7467dd59678125f34c874916146
bstocker <boris.stocker@gmail.com>
2025-04-07 12:52:08 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/<path:valeurs>')
-def somme(valeurs):
+def exercice(valeurs):
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
-    res = sum(liste_nombres)
-    return str(res)
+    resultat = 0
+    for n in liste_nombres:
+        resultat = resultat + n
+    return str(resultat)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
